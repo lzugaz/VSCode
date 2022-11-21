7812cdff426b61b35ffdd506707593adcfd0490f
lzugaz <93231358+lzugaz@users.noreply.github.com>
2022-11-20 21:38:06 -0600

    EDIT

---
+++ LeetCode/Permutations.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        
+        LinkedList <Integer>Permutations = new LinkedList<Integer>();
+        
+
+        return null;//temp return 
+        
+    }
+}
