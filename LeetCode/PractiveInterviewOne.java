a35e27e1bea797ca4a402a048d443a24edb7ed4e
lzugaz <93231358+lzugaz@users.noreply.github.com>
2022-11-24 13:39:42 -0600

    11/24/2022

---
+++ LeetCode/PractiveInterviewOne.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class PractiveInterviewOne {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+    }
+    public String removeKdigits(String num, int k) {
+        int len = num.length(), hi = 0, left = len - k;
+        if (len == k) return "0";
+        char[] res = new char[len];
+        for (int i = 0; i < len; i++) {
+            while (len - i + hi > left && k > 0 && hi > 0 && res[hi - 1] > num.charAt(i)) {
+                hi--;
+                k--;
+            }
+            if (hi < left) res[hi++] = num.charAt(i);
+        }
+        int lo = 0;
+        while (lo < hi && res[lo] == '0') lo++;
+        return lo == hi ? "0" : new String(res, lo, hi - lo);
+    }
+}
