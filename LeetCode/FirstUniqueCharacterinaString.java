9f968849e0db3e535a8efd7ef8753cf9b94df79b
lzugaz <93231358+lzugaz@users.noreply.github.com>
2022-11-29 00:23:38 -0500

    Fist test

--- LeetCode/FirstUniqueCharacterinaString.java
+++ LeetCode/FirstUniqueCharacterinaString.java
@@ -3,16 +3,17 @@ import java.util.Map;
 
 public class FirstUniqueCharacterinaString {
     public int firstUniqChar(String s) {
-        Map<Integer, Character> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         for(int i = 0; i < s.length(); i++){
-            map.put(i, s.charAt(i));
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         for(int i = 0; i < s.length(); i++){
             Character n = s.charAt(i);
-            if(map.containsKey(n)){
-                
+            if(map.get(s.charAt(i)) == 1){
+                return i;
             }
         }
-        return 0; // temp
+        return -1; // temp
     }
 }
