41de578d27ad98c0674e872629ac87b547bd22d9
lzugaz <93231358+lzugaz@users.noreply.github.com>
2022-11-29 16:15:34 -0500

    11/29/2022

---
+++ LeetCode/SubtracttheProductandSumofDigitsofanInteger.java
@@ -0,0 +1,12 @@
+public class SubtracttheProductandSumofDigitsofanInteger {
+    public int subtractProductAndSum(int n) {
+        int p=1,s=0;
+        while(n!=0){
+            int rem=n%10;
+            p*=rem;
+            s+=rem;
+            n/=10;
+        }
+        return p-s;
+    }
+}
